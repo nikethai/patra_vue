1406dcafbdc06f8c5b219490cf3c58b07cea699f
nikethai <46261937+nikethai@users.noreply.github.com>
2020-03-19 11:39:54 +0700

    add PWA

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify';
 import Vuetify from 'vuetify/lib'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,24 @@ module.exports = {
   },
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa:{
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions:{
+      navigateFallback: '/index.html',
+      runtimeCaching:[
+        {
+          urlPattern: new RegExp('^'),
+          handler: 'networkFirst',
+          options:{
+            networkTimeoutSeconds: 20,
+            cacheName: 'api-cache',
+            cacheableResponse:{
+              statuses: [0,200]
+            }
+          }
+        }
+      ]
+    }
+  }
 }
