4dcbc1530d4e544e5507979fcabc534b9ff18bdb
minhtus <mitu9811@gmail.com>
2020-03-29 21:52:06 +0700

    gh-pages and env configuration

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nikethai/patra_vue.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "patra_vue" : "/",
   configureWebpack: {
     devtool: "source-map"
   },
