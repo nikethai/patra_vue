057e9a9ccd486f15618e4932ba23913a22f8d0ae
Thomas T Thai <thong.thai7@gmail.com>
2020-03-29 17:03:28 +0700

    add gh-paage

---
+++ dist/precache-manifest.e3a367d21947cbd4348df7a4abfe0237.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff50e121ddbfae8e67e2",
+    "url": "/patra_vue/css/about.c47a1999.css"
+  },
+  {
+    "revision": "37bf861e70a8fbc3ba06",
+    "url": "/patra_vue/css/app.d0f13a8c.css"
+  },
+  {
+    "revision": "6e325141521578c8c906",
+    "url": "/patra_vue/css/chunk-vendors.989cc9ba.css"
+  },
+  {
+    "revision": "ca820c7e402b005be14f31b3a773b78e",
+    "url": "/patra_vue/index.html"
+  },
+  {
+    "revision": "ff50e121ddbfae8e67e2",
+    "url": "/patra_vue/js/about.7eaf1255.js"
+  },
+  {
+    "revision": "37bf861e70a8fbc3ba06",
+    "url": "/patra_vue/js/app.b508fcca.js"
+  },
+  {
+    "revision": "6e325141521578c8c906",
+    "url": "/patra_vue/js/chunk-vendors.abe7b660.js"
+  },
+  {
+    "revision": "a8c55e0f6eed88ca1d027e14390c1864",
+    "url": "/patra_vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/patra_vue/robots.txt"
+  }
+]);
