370c38376ecd2078e7bf07646a37b32b0c645576
minhtus <mitu9811@gmail.com>
2020-04-06 08:09:13 +0700

    Deployment

---
+++ precache-manifest.a2c25af1f9f279beddf1fc2c5765139a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab94e8debe4f206d9f16",
+    "url": "/patra_vue/css/app.b3043de1.css"
+  },
+  {
+    "revision": "8566a0a597896fb1389e",
+    "url": "/patra_vue/css/chunk-46348634.0dcfb759.css"
+  },
+  {
+    "revision": "66197b91042f138019c9",
+    "url": "/patra_vue/css/chunk-7907016f.06a75bc5.css"
+  },
+  {
+    "revision": "7bcda7077e7e8ac7ed83",
+    "url": "/patra_vue/css/chunk-8b44bf4c.ecddf955.css"
+  },
+  {
+    "revision": "63fd27da79037b2ee882",
+    "url": "/patra_vue/css/chunk-cef95f10.9f513c3c.css"
+  },
+  {
+    "revision": "c384b8383c820ad1d670",
+    "url": "/patra_vue/css/chunk-vendors.c85ddef6.css"
+  },
+  {
+    "revision": "09da910647973bd148ad931261534587",
+    "url": "/patra_vue/index.html"
+  },
+  {
+    "revision": "ab94e8debe4f206d9f16",
+    "url": "/patra_vue/js/app.6277c725.js"
+  },
+  {
+    "revision": "224ba6c97506c4a73844",
+    "url": "/patra_vue/js/chunk-2d0cba86.c798966f.js"
+  },
+  {
+    "revision": "8566a0a597896fb1389e",
+    "url": "/patra_vue/js/chunk-46348634.a8174e5f.js"
+  },
+  {
+    "revision": "66197b91042f138019c9",
+    "url": "/patra_vue/js/chunk-7907016f.a54dcefe.js"
+  },
+  {
+    "revision": "7bcda7077e7e8ac7ed83",
+    "url": "/patra_vue/js/chunk-8b44bf4c.f4d8516c.js"
+  },
+  {
+    "revision": "63fd27da79037b2ee882",
+    "url": "/patra_vue/js/chunk-cef95f10.c4e5d53a.js"
+  },
+  {
+    "revision": "c384b8383c820ad1d670",
+    "url": "/patra_vue/js/chunk-vendors.aa3a1046.js"
+  },
+  {
+    "revision": "a8c55e0f6eed88ca1d027e14390c1864",
+    "url": "/patra_vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/patra_vue/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/patra_vue/precache-manifest.a2c25af1f9f279beddf1fc2c5765139a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "patra"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
+
+workbox.routing.registerRoute(/^/, new workbox.strategies.NetworkFirst({ "cacheName":"api-cache","networkTimeoutSeconds":20, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
